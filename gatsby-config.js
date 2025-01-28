85d82cb7a1f028cbc0d221ccb1b2eff7bc0e80a1
Andrey Azevedo <andreyaze@gmail.com>
2025-01-28 15:46:41 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bloco-de-notas`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
